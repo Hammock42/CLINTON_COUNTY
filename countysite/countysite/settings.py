0ba5d7f64f73cdf9536a05ee69b6f9955dd50b87
Hamad Hamdi <Hammock42@protonmail.com>
2024-08-10 14:44:41 +0200

    Added vercel json

--- countysite/countysite/settings.py
+++ countysite/countysite/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g2to+5el36c@2moctlxk*j#&ng$6*$xmld3(i(8t!vzph2tv-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
