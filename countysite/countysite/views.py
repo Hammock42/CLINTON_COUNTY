7e4ac18919876d5a6e1d64de4d1ac9e7bae43daf
Hamad Hamdi <Hammock42@protonmail.com>
2025-12-18 06:10:44 +0200

    Fixed link errors

--- countysite/countysite/views.py
+++ countysite/countysite/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.conf import settings
-from django.core.mail import send_mail
-from .forms import ContactForm
 
 
 
@@ -19,24 +16,3 @@ def privacy_policy(request):
 
 def legal_statement(request):
     return render(request, 'legal-statement.html')
-
-def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data.get('name')
-            email = form.cleaned_data.get('email')
-            subject = form.cleaned_data.get('subject')
-            message = form.cleaned_data.get('message')
-
-            send_mail(
-                subject,
-                message,
-                email,
-                ["hamadhamdi13831@gmail.com"],
-                fail_silently=False,
-            )
-            return render(request, 'contact.html', {'form': form, 'success': True})
-    else:
-        form = ContactForm()
-    return render(request, 'contact.html', {'form': form})
